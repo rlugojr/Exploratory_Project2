5057c2c10a5fa23fae04ce842928ba1335d1aaf0
Ray Lugo, Jr <rlugojr@gmail.com>
2017-02-05 16:53:39 -0500

    Plot5 update
    Changed line color and width
    Save to PNG

--- plot5.R
+++ plot5.R
@@ -2,4 +2,4 @@
 baltimoreEmissionsOnRoad <- filter(baltimoreData, type == "ON-ROAD")
 baltimoreEmissionsOnRoadYear <- aggregate(Emissions ~ year + type, baltimoreEmissionsOnRoad, sum)
 
-ggplot(baltimoreEmissionsOnRoadYear, aes(year,Emissions)) + geom_line(lwd=2) + ggtitle("Emissions from Motor Vehicles in Baltimore City, MD") + xlab("Year") + ylab("Total PM2.5")
+ggplot(baltimoreEmissionsOnRoadYear, aes(year,Emissions)) + geom_line(color = "blue", size = 2) + ggtitle("Emissions from Motor Vehicles in Baltimore City, MD") + xlab("Year") + ylab("Total PM2.5") + ggsave("plot5.png")
