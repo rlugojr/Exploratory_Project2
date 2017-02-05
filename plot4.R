62ca2773ae76cae02d75b82f6552c3e53ab0b1c0
Ray Lugo, Jr <rlugojr@gmail.com>
2017-02-05 16:18:17 -0500

    Plot4 added
    subset of data using grepl to filter data.
    initial plot created.

---
+++ plot4.R
@@ -0,0 +1,8 @@
+
+coalCombSCC <- filter(SCC, grepl("Coal", SCC$EI.Sector)) %>% select(SCC)
+
+coalCombData <- filter (NEI, NEI$SCC %in% coalCombSCC$SCC)
+
+ggplot(coalCombData, aes(factor(year), Emissions, color=type)) + geom_point(position = position_dodge(0.5))
+
+
