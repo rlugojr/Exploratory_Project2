5dbe8b39bddccaccfebc9aaa416c7d8811457cbb
Ray Lugo, Jr <rlugojr@gmail.com>
2017-02-05 16:35:51 -0500

    Final plot4
    Added Y label
    modified title label
    export to PNG

--- plot4.R
+++ plot4.R
@@ -1,8 +1,6 @@
 
 coalCombSCC <- filter(SCC, grepl("Coal", SCC$EI.Sector)) %>% select(SCC)
 
-coalCombData <- filter (NEI, NEI$SCC %in% coalCombSCC$SCC)
+coalCombData <- filter(NEI, NEI$SCC %in% coalCombSCC$SCC)
 
-ggplot(coalCombData, aes(factor(year), Emissions, color=type)) + geom_point(position = position_dodge(0.5)) + ggtitle("Emissions from Coal Sources") + xlab("Year")
-
-
+ggplot(coalCombData, aes(factor(year), Emissions, color = type)) + geom_point(position = position_dodge(0.5)) + ggtitle("Emissions from Coal Sources Across the U.S.") + xlab("Year") + ylab("Total PM2.5") + ggsave("plot4.png")
