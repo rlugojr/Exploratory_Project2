af7e83c1fce4c1986e986ffee65c90cddc1ba2b5
Ray Lugo, Jr <rlugojr@gmail.com>
2017-02-05 17:55:33 -0500

    Make file
    Added make.R as the main build file that runs environment setup and plot
    generation scripts.

---
+++ make.R
@@ -0,0 +1,24 @@
+
+print("Making environment")
+source('setup_env.r')
+
+print("Making Plot1")
+source('plot1.R')
+
+print("Making Plot2")
+source('plot2.R')
+
+print("Making Plot3")
+source('plot3.R')
+
+print("Making Plot4")
+source('plot4.R')
+
+print("Making Plot5")
+source('plot5.R')
+
+print("Making Plot6")
+source('plot6.R')
+
+print(paste("finished at :", Sys.time()))
+print("Completed!")
